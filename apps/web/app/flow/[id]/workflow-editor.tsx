3634a7106dfc7ff04e0cfa2fb89867ab7030aea3
tukeceshi <tukeceshi@icloud.com>
2025-02-10 23:19:26 +0100

    Add execute workflow button to workflow editor

--- apps/web/app/flow/[id]/workflow-editor.tsx
+++ apps/web/app/flow/[id]/workflow-editor.tsx
@@ -104,6 +104,27 @@ export function WorkflowEditor({ initialWorkflowGraph, onWorkflowChange }: Workf
               />
             </svg>
           </Button>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              // Execute functionality will be implemented later
+            }}
+            size="icon"
+            className="absolute top-4 right-4 z-50 rounded-full shadow-lg"
+            title="Execute Workflow"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347c-.75.412-1.667-.13-1.667-.986V5.653Z" />
+            </svg>
+          </Button>
         </ReactFlow>
       </div>
 
