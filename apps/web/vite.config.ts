0ed77ffc7e8adbea0aa879b50277f09f19e80aa6
tukeceshi <tukeceshi@icloud.com>
2025-04-21 23:12:25 +0200

    Fix build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,22 +2,13 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Empty for now but advanced configuration can be setup
-const ReactCompilerConfig = {};
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@lib": path.resolve(__dirname, "./lib"),
+      "@dafthunk/types": path.resolve(__dirname, "../../packages/types/src"),
     },
   },
 });
