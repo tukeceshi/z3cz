2b24d7751fd1cae2f0b4dd5a1f68df22d5d011a2
tukeceshi <tukeceshi@icloud.com>
2025-05-18 10:49:24 +0200

    fix(web): use correct timer type for debounce in browser environment

--- apps/web/src/utils/utils.ts
+++ apps/web/src/utils/utils.ts
@@ -22,7 +22,7 @@ export const debounce = <T extends (...args: Parameters<T>) => ReturnType<T>>(
   func: T,
   wait: number
 ): ((...args: Parameters<T>) => void) => {
-  let timeout: NodeJS.Timeout;
+  let timeout: ReturnType<typeof setTimeout>;
   return (...args: Parameters<T>) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), wait);
