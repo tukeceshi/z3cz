b3bc14f82a283fd3405b1e6ba1d4dac2e013c50d
tukeceshi <tukeceshi@icloud.com>
2026-08-07 14:50:27 +0800

    优化画布资源显示，适配大量资源显示场景

--- tmp-find-api.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for d in /proc/[0-9]*; do
-  cmd=$(tr '\0' ' ' < "$d/cmdline" 2>/dev/null) || continue
-  case "$cmd" in
-    *tsx*|*server.ts*|*watch*)
-      echo "$d $cmd"
-      ;;
-  esac
-done
