a82945f540fe6019b18e212ebf893ea78f6f5671
tukeceshi <tukeceshi@icloud.com>
2026-08-07 19:05:48 +0800

    修复在线更新

--- apps/api/src/db/migrations/0057_add_minimax_speech_models.sql
+++ apps/api/src/db/migrations/0057_add_minimax_speech_models.sql
@@ -7,7 +7,6 @@ INSERT INTO "platform_ai_models" (
   "display_name",
   "modality",
   "platform_enabled",
-  "provider_model_id",
   "parameter_rules",
   "sort_order",
   "group_id"
@@ -17,7 +16,6 @@ INSERT INTO "platform_ai_models" (
     'MiniMax Speech 2.8 HD',
     'audio',
     true,
-    'speech-2.8-hd',
     '{"schemaVersion":1,"promptMaxChars":5000,"generationFields":[{"name":"speed","apiName":"voice_setting.speed","type":"number","description":"Speech speed multiplier","default":1},{"name":"vol","apiName":"voice_setting.vol","type":"number","description":"Speech volume","default":1},{"name":"pitch","apiName":"voice_setting.pitch","type":"number","description":"Speech pitch adjustment","default":0},{"name":"emotion","apiName":"voice_setting.emotion","type":"string","description":"Speech emotion style","default":"neutral","enumValues":["happy","sad","angry","fearful","disgusted","surprised","neutral"]},{"name":"voice_id","apiName":"voice_setting.voice_id","type":"string","description":"Default voice identifier","default":"male-qn-qingse","hidden":true}]}'::jsonb,
     96,
     'minimax'
@@ -27,7 +25,6 @@ INSERT INTO "platform_ai_models" (
     'MiniMax Speech 2.8 Turbo',
     'audio',
     true,
-    'speech-2.8-turbo',
     '{"schemaVersion":1,"promptMaxChars":5000,"generationFields":[{"name":"speed","apiName":"voice_setting.speed","type":"number","description":"Speech speed multiplier","default":1},{"name":"vol","apiName":"voice_setting.vol","type":"number","description":"Speech volume","default":1},{"name":"pitch","apiName":"voice_setting.pitch","type":"number","description":"Speech pitch adjustment","default":0},{"name":"emotion","apiName":"voice_setting.emotion","type":"string","description":"Speech emotion style","default":"neutral","enumValues":["happy","sad","angry","fearful","disgusted","surprised","neutral"]},{"name":"voice_id","apiName":"voice_setting.voice_id","type":"string","description":"Default voice identifier","default":"male-qn-qingse","hidden":true}]}'::jsonb,
     97,
     'minimax'
